ea67d98fd6e2d29690b380852d5567c1a162f90a
marianna <marianna@primeflow.com>
2020-12-09 10:27:04 +0100

    moving the snake

---
+++ snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+UP = 90.0
+DOWN = 270.0
+RIGHT = 0.0
+LEFT = 180.0
+
+class Snake:
+
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        for i in range(3):
+            new_tile = Turtle()
+            new_tile.shape("square")
+            new_tile.color("white")
+            new_tile.penup()
+            new_tile.setposition(x=-20*i, y=0)
+            self.snake.append(new_tile)
+
+    def move(self):
+        for i in range(len(self.snake) - 1, 0, -1):
+            new_x = self.snake[i - 1].xcor()
+            new_y = self.snake[i - 1].ycor()
+            self.snake[i].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
